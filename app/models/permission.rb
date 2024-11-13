52ca009d82f8ff597e7e2f6fa68bfa8a8edd06d5
J4sTFly <alex.hrynts@gmail.com>
2024-11-13 22:22:25 +0300

    Add unique index to permissions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -9,6 +9,10 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+# Indexes
+#
+#  index_permissions_on_controller_and_action  (controller,action) UNIQUE
+#
 class Permission < ApplicationRecord
   has_and_belongs_to_many :subscription_plans, optional: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_12_171341) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_13_191700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,12 +25,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_12_171341) do
     t.index ["subscription_plan_id"], name: "index_api_keys_on_subscription_plan_id"
   end
 
+  create_table "data_migrations", primary_key: "version", id: :string, force: :cascade do |t|
+  end
+
   create_table "permissions", force: :cascade do |t|
     t.integer "api_version", null: false
     t.string "controller", null: false
     t.string "action", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["controller", "action"], name: "index_permissions_on_controller_and_action", unique: true
   end
 
   create_table "permissions_subscription_plans", id: false, force: :cascade do |t|
