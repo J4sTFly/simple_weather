15cf14d070dd1590a1953c55280247211a53b900
J4sTFly <alex.hrynts@gmail.com>
2024-11-15 01:57:35 +0300

    Add context to specs

--- spec/requests/api/v1/weather_controller_spec.rb
+++ spec/requests/api/v1/weather_controller_spec.rb
@@ -5,97 +5,108 @@ RSpec.describe "Api::V1::ApiKeysControllers", type: :request do
   let!(:api_key) { create :api_key }
 
   describe "GET /index" do
-    let(:valid_query) do
-      {
-        key: api_key.value,
-        city: 'London',
-        days: 1
-      }
+    context "with valid params" do
+      let(:valid_query) do
+        {
+          key: api_key.value,
+          city: 'London',
+          days: 1
+        }
+      end
+
+      it 'returns weather forecast' do
+        get api_v1_weather_index_path, params: valid_query
+
+        expect(response).to be_successful
+        expect(JSON.parse(response.body).length).to eq(valid_query[:days])
+        expect(JSON.parse(response.body).first['date'].to_date).to eq(Date.today)
+      end
     end
 
-    it 'returns weather forecast with valid query_params' do
-      get api_v1_weather_index_path, params: valid_query
+    context 'with invalid params' do
+      context 'with days number exceeding the subcription plan range' do
+        let(:exceed_max_days) do
+          {
+            key: api_key.value,
+            city: 'London',
+            days: subscription_plan.forecast_days + 1
+          }
+        end
 
-      expect(response).to be_successful
-      expect(JSON.parse(response.body).length).to eq(valid_query[:days])
-      expect(JSON.parse(response.body).first['date'].to_date).to eq(Date.today)
-    end
+        it 'returns 400 with Days error' do
+          get api_v1_weather_index_path, params: exceed_max_days
 
+          expect(response).to be_a_bad_request
+          expect(response.body).to eq({ errors: [ "Days #{I18n.t('activemodel.errors.models.api/v1/weather_base_form.attributes.days.not_in_plan_forecast_range')}" ] }.to_json)
+        end
+      end
 
-    let(:exceed_max_days) do
-      {
-        key: api_key.value,
-        city: 'London',
-        days: subscription_plan.forecast_days + 1
-      }
-    end
-    it '400 when forecast days number is greater than subscription plan allows' do
-      get api_v1_weather_index_path, params: exceed_max_days
+      context 'with redurant params' do
+        let(:too_many_params) do
+          {
+            key: api_key.value,
+            city: 'London',
+            lat: 12.32
+          }
+        end
 
-      expect(response).to be_a_bad_request
-      expect(response.body).to eq({ errors: [ "Days #{I18n.t('activemodel.errors.models.api/v1/weather_base_form.attributes.days.not_in_plan_forecast_range')}" ] }.to_json)
-    end
+        it 'returns 400 with Incorrect Params Error' do
+          get api_v1_weather_index_path, params: too_many_params
 
-
-    let(:too_many_params) do
-      {
-        key: api_key.value,
-        city: 'London',
-        lat: 12.32
-      }
-    end
-    it '400 when too many params provided' do
-      get api_v1_weather_index_path, params: too_many_params
-
-      expect(response).to be_a_bad_request
-      expect(response.body).to eq({ errors: [ I18n.t('activemodel.errors.models.api/v1/weather_base_form.attributes.base.incorrect_params_provided') ] }.to_json)
+          expect(response).to be_a_bad_request
+          expect(response.body).to eq({ errors: [ I18n.t('activemodel.errors.models.api/v1/weather_base_form.attributes.base.incorrect_params_provided') ] }.to_json)
+        end
+      end
     end
   end
 
   describe "POST /schedule" do
-    let(:valid_params) do
-      {
-        key: api_key.value,
-        city: "London",
-        days: 1,
-        time: "11:00",
-        weekdays: "MO",
-        email: "sample@mail.com"
-      }
+    context 'with valid params' do
+      let(:valid_params) do
+        {
+          key: api_key.value,
+          city: "London",
+          days: 1,
+          time: "11:00",
+          weekdays: "MO",
+          email: "sample@mail.com"
+        }
+      end
+
+      it 'returns schedule and schedules a new sidekiq workers if params are valid' do
+        post schedule_api_v1_weather_index_path, params: valid_params
+
+        time = valid_params[:time].to_time
+        ice_cube_rule = IceCube::Rule.weekly.day(1).hour_of_day(time.hour).minute_of_hour(time.min)
+
+        expect(response).to be_successful
+        expect(response.body).to eq({ message: I18n.t('api.v1.weather.schedule.scheduled', schedule: ice_cube_rule.to_s) }.to_json)
+        expect(Sidekiq::ScheduledSet.new.size).not_to be_zero
+      end
     end
 
-    it 'returns schedule and schedules a new sidekiq workers if params are valid' do
-      post schedule_api_v1_weather_index_path, params: valid_params
+    context 'with invalid parmas' do
+      let(:invalid_params) do
+        {
+          key: api_key.value,
+          city: "London",
+          days: 1,
+          time: "25:60",
+          weekdays: "MO, TUESDAY",
+          email: "invalid mail"
+        }
+      end
 
-      time = valid_params[:time].to_time
-      ice_cube_rule = IceCube::Rule.weekly.day(1).hour_of_day(time.hour).minute_of_hour(time.min)
+      it 'returns 400' do
+        post schedule_api_v1_weather_index_path, params: invalid_params
 
-      expect(response).to be_successful
-      expect(response.body).to eq({ message: I18n.t('api.v1.weather.schedule.scheduled', schedule: ice_cube_rule.to_s) }.to_json)
-      expect(Sidekiq::ScheduledSet.new.size).not_to be_zero
-    end
-
-
-    let(:invalid_params) do
-      {
-        key: api_key.value,
-        city: "London",
-        days: 1,
-        time: "25:60",
-        weekdays: "MO, TUESDAY",
-        email: "invalid mail"
-      }
-    end
-
-    it '400 when params are invalid' do
-      post schedule_api_v1_weather_index_path, params: invalid_params
-
-      expect(response).to be_a_bad_request
-      expect(response.body).to eq({
-        errors: [ 'Email is invalid',
-                  "Time #{I18n.t('activemodel.errors.models.api/v1/schedule_weather_form.attributes.time.invalid_format')}",
-                  "Weekdays #{I18n.t('activemodel.errors.models.api/v1/schedule_weather_form.attributes.weekdays.invalid_format')}" ]
-      }.to_json)
+        expect(response).to be_a_bad_request
+        expect(response.body).to eq({
+          errors: [ 'Email is invalid',
+                    "Time #{I18n.t('activemodel.errors.models.api/v1/schedule_weather_form.attributes.time.invalid_format')}",
+                    "Weekdays #{I18n.t('activemodel.errors.models.api/v1/schedule_weather_form.attributes.weekdays.invalid_format')}" ]
+        }.to_json)
+      end
     end
   end
 end
