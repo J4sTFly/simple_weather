7a253254d6ebe8d6776adf5dce3760eefc0c739b
Alex Hryntsaliou <103586958+J4sTFly@users.noreply.github.com>
2024-11-15 02:11:58 +0300

    Tests improvements, Comments cleanup (#5)
    * Add context to specs
    
    * Remove odd comments

--- app/workers/mail_weather_data_worker.rb
+++ app/workers/mail_weather_data_worker.rb
@@ -4,6 +4,7 @@ class MailWeatherDataWorker
   def perform(params)
     params = JSON.parse(params).symbolize_keys
     result = Api::V1::RetrieveWeatherDataService.new.call(params[:service_query].symbolize_keys)
+
     if result
       data = Api::V1::WeatherDataSerializer.new(result).serialized_data
       WeatherDataMailer.with(email: params[:email], data:).weather_data_email.deliver_now
