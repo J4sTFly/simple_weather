9cff6ab1be906bb58b8b5600080a30ddfab74455
Alex Hryntsaliou <103586958+J4sTFly@users.noreply.github.com>
2024-11-17 23:42:58 +0300

    Remove Rails credentials for better testing during the review (#11)

--- app/services/api/v1/retrieve_weather_data_service.rb
+++ app/services/api/v1/retrieve_weather_data_service.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     class RetrieveWeatherDataService
       include Rails.application.routes.url_helpers
-      WEATHERAPI_TOKEN = Rails.application.credentials.dig(:secrets, :weatherapi_token)
+      # ENV for testing purposes only. In real app should be taken from credentials
+      # Rails.application.credentials.dig(:secrets, :weatherapi_token)
+      WEATHERAPI_TOKEN = ENV.fetch("WEATHERAPI_TOKEN")
 
       def call(query)
         response = Faraday.get(weather_api_url, query.merge(key: WEATHERAPI_TOKEN))
