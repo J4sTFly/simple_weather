7ec85109d28659a6299fe45ae4fb23f595bbb822
J4sTFly <alex.hrynts@gmail.com>
2024-11-18 01:40:24 +0300

    - Fix compose migration errors; - Create db on compose start

--- db/migrate/20241111090426_create_subscription_plans.rb
+++ db/migrate/20241111090426_create_subscription_plans.rb
@@ -2,6 +2,7 @@ class CreateSubscriptionPlans < ActiveRecord::Migration[7.2]
   def change
     create_table :subscription_plans do |t|
       t.string :name, null: false
+      t.integer :forecast_days, null: false
       t.timestamps
     end
   end
